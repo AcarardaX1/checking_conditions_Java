ac882b0fc38b508949cb1aa9bcef9b857f76dd91
Arda Acar <108298837+AcarardaX1@users.noreply.github.com>
2023-09-26 16:38:25 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		String internet_branch_button = "Internet Subesi"; 
+		
+		double EUR_USD = 1.25;
+		int EUR_TRY = 29;
+		int USD_TRY = 28;
+		int expiry = 48;
+		boolean down = false;
+		boolean up = true;
+
+		
+
+		System.out.println(internet_branch_button);																		
+	
+		if(EUR_TRY > USD_TRY) {
+			System.out.println("EUR is worthier than USD and TRY for today");
+		}
+		else {
+			System.out.println("USD is worthier than EUR and TRY for today");
+			
+		}
+			
+			
+			
+		
+		
+		
+	}
+
+}
